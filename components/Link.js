579a8d5e006db02851550990e860df48967fe027
Jeff Weimer <jweimer@happymoney.com>
2020-10-29 21:48:51 -0700

    update

--- components/Link.js
+++ components/Link.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
-import Link from 'next/Link'
+import Link from 'next/link'
 
 const LinkElement = props => {
   const { as, children, external, href, passHref } = props
